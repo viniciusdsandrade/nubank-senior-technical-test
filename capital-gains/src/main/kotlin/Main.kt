ec5cb05f4574884f7943f60e2012200287dfc08d
andrade <viniciusdsandrade0662@gmail.com>
2025-12-18 12:39:51 -0300

    feat: implement tax calculation functionality in main method

--- capital-gains/src/main/kotlin/Main.kt
+++ capital-gains/src/main/kotlin/Main.kt
@@ -1,8 +1,20 @@
-fun main() {
-    val name = "Kotlin"
-    println("Hello, $name!")
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import model.Operation
+import model.TaxResult
+import service.CapitalGainCalculator
 
-    for (i in 1..5) {
-        println("i = $i")
+fun main() {
+    val mapper = jacksonObjectMapper()
+    val calculator = CapitalGainCalculator()
+
+    val reader = System.`in`.bufferedReader()
+    while (true) {
+        val line = reader.readLine() ?: break
+        if (line.isBlank()) continue
+
+        val operations: List<Operation> = mapper.readValue(line)
+        val taxes: List<TaxResult> = calculator.calculateTaxes(operations)
+        println(mapper.writeValueAsString(taxes))
     }
 }
